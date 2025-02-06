0f06b33080db4c13cbffe315de8d8f2c97c4b58e
Jaber <rantisijaber@gmail.com>
2025-02-06 12:48:27 -0500

    Began working on Thymeleaf frontend

--- src/main/java/com/rantisi/library/service/LibraryService.java
+++ src/main/java/com/rantisi/library/service/LibraryService.java
@@ -9,19 +9,24 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+// Creates the methods and business logic that will be used in the controller
 @Service
 public class LibraryService {
 
+    // Autowiring / Dependency Injection (Constructor injection)
     private final LibraryRepo libraryRepo;
+
 
     public LibraryService(LibraryRepo libraryRepo) {
         this.libraryRepo = libraryRepo;
     }
 
+    // Finds all the books in the database
     public List<Book> findAll() {
         return libraryRepo.findAll();
     }
 
+    // Finds book by ID
     public Book findById(Integer id, boolean isAPI) {
         Optional<Book> book = libraryRepo.findById(id);
         if (isAPI) {
@@ -31,10 +36,11 @@ public class LibraryService {
         }
     }
 
+    // Creates a new book, saves it to the database, and can be accessed via the API
     public void create(Book book) {
         libraryRepo.save(book);
     }
-
+    // Updates the book, saves new information to the database, can be accessed via API
     public void update(Book book, Integer id) {
         if (!libraryRepo.existsById(id)) {
             throw new BookNotFoundException();
@@ -42,6 +48,8 @@ public class LibraryService {
         book.setId(id);
         libraryRepo.save(book);
     }
+
+    // Deletes the book, removes it from the database, can be accessed via API
     public void delete(Integer id) {
         if (!libraryRepo.existsById(id)) {
             throw new BookNotFoundException();
